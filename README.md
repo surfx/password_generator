a4fe7ea3758a0c253efe446e526c30bb46607d38
Emerson Shigueo Sugimoto <emerson.sugimoto@serpro.gov.br>
2024-01-16 11:06:42 -0300

    commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# password_generator
-password generator
+# Password generator
+
+Gerador de senhas em HTML, CSS e Javascript
+
+# Urls
+
+- [js shuffle](https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/)
+- [css reset](http://meyerweb.com/eric/tools/css/reset/)
+- [Flaticon](https://www.flaticon.com/br/)
+- [How TO - Range Sliders](https://www.w3schools.com/howto/howto_js_rangeslider.asp)
+- [Roboto - Google Fonts](https://fonts.google.com/specimen/Roboto)
+- [IDE Vscode](https://code.visualstudio.com)
+- [plugin Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
---
+++ assets/control.js
@@ -0,0 +1,85 @@
+let txtPassword = document.getElementById('txtPassword');
+
+let slider = document.getElementById("mySlider");
+let output = document.getElementById("spnValorSlider");
+output.innerHTML = slider.value; // Display the default slider value
+
+let chkCaracteresEspeciais = document.getElementById('chkCaracteresEspeciais');
+let chkNumeros = document.getElementById('chkNumeros');
+let chkMaiusculas = document.getElementById('chkMaiusculas');
+let chkMinusculas = document.getElementById('chkMinusculas');
+
+let spnMensagem = document.getElementById('spnMensagem');
+
+data = {
+    minusculas: [...'abcdefghijklmnopqrstuvwxyz'],
+    maiusculas: [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'],
+    numeros: [...'012345678909'],
+    caracteresEspeciais: [...'!@#$%&*+-*/ ']
+}
+
+// https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
+const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+function showMsg(mensagem) {
+    spnMensagem.innerHTML = mensagem;
+    setTimeout(() => {
+        spnMensagem.innerHTML = '';
+    }, 1000);
+}
+
+function gerarSenha() {
+    txtPassword.value = '';
+    let size = slider.value;
+    if (size <= 0) { return; }
+    let dbChars = [];
+    if (chkNumeros.checked) {
+        dbChars = [...dbChars, ...data.numeros];
+    }
+    if (chkCaracteresEspeciais.checked) {
+        dbChars = [...dbChars, ...data.caracteresEspeciais];
+    }
+    if (chkMaiusculas.checked) {
+        dbChars = [...dbChars, ...data.maiusculas];
+    }
+    if (chkMinusculas.checked) {
+        dbChars = [...dbChars, ...data.minusculas];
+    }
+    if (dbChars.length <= 0) { return; }
+
+    let rt = '';
+    while (rt.length <= size) {
+        let pos = Math.floor(Math.random() * dbChars.length);
+        dbChars = shuffle(dbChars);
+        rt += dbChars[pos];
+    }
+    txtPassword.value = rt;
+}
+
+function copiarParaAreaTransferencia(){
+    navigator.clipboard.writeText(txtPassword.value);
+    showMsg('Copiado para a área de transferência');
+}
+
+chkCaracteresEspeciais.onchange = gerarSenha;
+chkNumeros.onchange = gerarSenha;
+chkMaiusculas.onchange = gerarSenha;
+chkMinusculas.onchange = gerarSenha;
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+    gerarSenha();
+}
+
+document.getElementById('btnRefresh').onclick = gerarSenha;
+document.getElementById('btnCopy').onclick = copiarParaAreaTransferencia;
+document.getElementById('btnCopiar').onclick = copiarParaAreaTransferencia;
+
+gerarSenha();
